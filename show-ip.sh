587e8245254732c9d9412460f3d32112091b41da
Jerrylum <serverofjerry@gmail.com>
2022-06-28 05:41:50 +0800

    Change comment

--- show-ip.sh
+++ show-ip.sh
@@ -2,6 +2,7 @@
 
 # display the IP address of the current host
 
+# to start this script automatically at boot
 # type "sudo nano /etc/xdg/lxsession/LXDE-pi/autostart"
 # and add the following line:
 # @lxterminal --command "/home/cityurbc/scripts/show-ip.sh"
